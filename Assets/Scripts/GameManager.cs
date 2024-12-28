d99e32eb05427f3e662a075450d43b2ba2f49611
Rohan <blockytube1@gmail.com>
2024-12-28 13:20:02 -0800

    created slightly different physics system and added 1 type of tile

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    public float CoulumbicConstant = 0.5f;
+
+    private void Awake()
+    {
+        if(Instance = null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/MagnetBehaviour.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagnetBehaviour : MonoBehaviour
+{
+    public float charge;
+    [SerializeField] float range;
+
+    Collider2D[] colliders;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        colliders = Physics2D.OverlapCircleAll(transform.position, range);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].TryGetComponent<MagnetBehaviour>(out MagnetBehaviour other) && other.transform.parent != transform.parent && other.transform != transform){
+                Vector2 dir = transform.position - other.transform.position;
+                float dist = dir.magnitude;
+                other.GetComponent<Rigidbody2D>().AddForce(-dir.normalized * charge * other.charge * 0.5f / dist);
+            }
+        }
+    }
+
+    public float getCharge()
+    {
+        return charge;
+    }
+}
