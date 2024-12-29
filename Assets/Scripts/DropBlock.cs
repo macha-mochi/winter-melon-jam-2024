73ed6b1eb1974b738ff1a3bbd9623ad7e53110fa
richardz403 <116985684+richardz403@users.noreply.github.com>
2024-12-28 22:19:26 -0800

    dropblock stuff

--- Assets/Scripts/DropBlock.cs
+++ Assets/Scripts/DropBlock.cs
@@ -21,19 +21,24 @@ public class DropBlock : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            freezeChildRigidbodies(currentPiece, false);
-            setAllChildMagnet(currentPiece, true);
-            currentPiece = null;
-            StartCoroutine(nextPiece());
+            if (currentPiece != null) {
+                freezeChildRigidbodies(currentPiece, false);
+                setAllChildMagnet(currentPiece, true);
+                currentPiece = null;
+                StartCoroutine(nextPiece());
+            }
+
         }
         else
         {
-            Vector3 worldPos = cam.ScreenToWorldPoint(Input.mousePosition);
-            currentPiece.transform.position = new Vector3(worldPos.x, currentPiece.transform.position.y, currentPiece.transform.position.z);
+            if (currentPiece != null) {
+                Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                currentPiece.transform.position = new Vector3(worldPos.x, currentPiece.transform.position.y, currentPiece.transform.position.z);
 
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                currentPiece.transform.Rotate(new Vector3(0, 0, 90));
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    currentPiece.transform.Rotate(new Vector3(0, 0, 90));
+                }
             }
         }
     }
