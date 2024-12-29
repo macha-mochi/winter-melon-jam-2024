0b85beb06e9fba68bff6e548cf00f92e0f9c4c37
richardz403 <116985684+richardz403@users.noreply.github.com>
2024-12-29 13:04:18 -0800

    present lost stuff

--- Assets/GameLevelManager.cs
+++ Assets/GameLevelManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameLevelManager : MonoBehaviour
@@ -8,6 +9,7 @@ public class GameLevelManager : MonoBehaviour
     public bool gameRunning = true;
     public int presentsLost = 0;
     public int maxLost = 12;
+    [SerializeField] TMP_Text lost;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,7 @@ public class GameLevelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        lost.text = "Presents Lost: " + presentsLost.ToString();
         if (presentsLost >= maxLost) { 
             gameRunning= false;
         }
