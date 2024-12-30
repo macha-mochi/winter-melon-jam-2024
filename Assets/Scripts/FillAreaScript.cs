13e81dce526367c9eec884c6f35e3e6bd2d9bafd
richardz403 <116985684+richardz403@users.noreply.github.com>
2024-12-30 02:54:15 -0800

    final push?

--- Assets/Scripts/FillAreaScript.cs
+++ Assets/Scripts/FillAreaScript.cs
@@ -20,7 +20,6 @@ public class FillAreaScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("AAA");
         if (collision.gameObject.tag == "Tile") {
             presents.Add(collision.gameObject);
         }
